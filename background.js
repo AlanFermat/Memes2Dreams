8866a21e95f7625407deb190c53eb2ed6ce96471
AlanFermat <zy19@rice.edu>
2019-09-21 15:26:15 -0500

    almost working

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 var rule = {
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostContains: 'youtube.com'},
+          pageUrl: {hostContains: 'twitter.com'},
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
@@ -27,15 +27,58 @@ chrome.runtime.onInstalled.addListener(function() {
     });
   });
 
-function addMemeKeyword(keyword) {
-  console.log("check 1");
-    chrome.storage.sync.get("meme", function(result) {
-      console.log("check2");
-      // var memes = result.key;
-      if (result.keyword === undefined) {
-        console.log("check3");
-        result.keyword = 0;
-        console.log(result);
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  for (var key in changes) {
+    var storageChange = changes[key];
+    if (key == "meme") {
+      var c = storageChange.newValue[key];
+      if (c == 1) {
+        display(1, c);
+      } else if (c >= 10) {
+        display(3, c);
+      } else if (c >= 5) {
+        display(2, c);
       }
+    }
+    console.log('Storage key "%s" in namespace "%s" changed. ' +
+                'Old value was "%s", new value is "%s".',
+                key,
+                namespace,
+                storageChange.oldValue,
+                storageChange.newValue);
+  }
+});
+
+
+// Parse the p tags elements in the loaded page and check if it contains the meme keywords
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log('I received the following DOM content:\n' + request.content);
+    var x = request.content;
+
+    console.log(x.length);
+
+    chrome.storage.sync.get("meme", function(result) {
+      console.log(result.meme);
+
+      var res = result.meme;
+
+      for (i = 0; i < x.length; i++) {
+        var h = x[i];
+
+        for (var keyword in res) {
+          if (h.includes(keyword)) {
+            res[keyword] += 1;
+          }
+        }
+      }
+
+      console.log(res);
+
+      chrome.storage.sync.set({"meme": res}, function(){
+          console.log("updated meme counts");
+        });
     });
-}
+  }
+);
---
+++ content.js
@@ -0,0 +1,24 @@
+window.onload =	function () {
+    // If the received message has the expected format...
+
+        // Call the specified callback, passing
+        // the web-page's DOM content as argument
+        // console.log(document.getElementsByTagName("P")[0].innerHTML);
+
+        var x = document.getElementsByTagName("P");
+        // chrome.runtime.sendMessage({"content": x});
+
+        var content = [];
+
+		var i;
+		for (i = 0; i < x.length; i++) {
+			x[i].style.backgroundColor = "red";
+			content.push(x[i].innerHTML);
+			// chrome.runtime.sendMessage({"content": x[i].innerHTML});
+
+		}
+		console.log(content);
+		chrome.runtime.sendMessage({"content": content});
+        // chrome.runtime.sendMessage({"content": document.getElementsByTagName("P")[0].innerHTML});
+
+};
