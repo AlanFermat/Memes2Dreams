56fc9418b479a81da8f223d4e712448309dca263
AlanFermat <zy19@rice.edu>
2019-09-22 01:43:01 -0500

    most updated

--- popup.js
+++ popup.js
@@ -1,14 +1,6 @@
-// let memeButton = document.getElementById("trackmeme_button");
-// let memeInput = document.getElementById("memeinput");
-
-// memeButton.onclick = function(element) {
-// 	var memeOutput = document.getElementById("memeoutput");
-// 	var meme = memeInput.value;
-// 	addMemeKeyword(meme);
-// 	memeOutput.innerHTML = meme;
-// };
-
 [].forEach.call(document.getElementsByClassName('tags-input'), function (el) {
+
+	let tagsList = [];
 	let hiddenInput = document.createElement('input'),
 		mainInput = document.createElement('input'),
 		tags = [];
@@ -18,36 +10,51 @@
 
 	mainInput.setAttribute('type', 'text');
 	mainInput.setAttribute('placeholder', 'Meme Keyword');
+	mainInput.setAttribute('style', 'padding: 10px; font-size: 15px');
 	mainInput.classList.add('main-input');
 
 	// Add tag on click
-	document.getElementById('thisbutton').onclick = function() {addTag()};
+	mainInput.addEventListener("keydown", function (arg) {
+		if (arg.keyCode === 13) {
+			addTag();
+		}
+	});
 
 	function addTag() {
-		let tag = {
-			text: mainInput.value,
-			element: document.createElement('span'),
-		};
+		if (tagsList.length < 5) {
 
-		tag.element.classList.add('tag');
-		tag.element.textContent = tag.text;
+			document.getElementById('warning').innerHTML = "";
 
-		let closeBtn = document.createElement('span'); // Create close button on tag
-		closeBtn.classList.add('close');
-		closeBtn.addEventListener('click', function() {
-			removeTag(tags.indexOf(tag));
-		})
-		tag.element.appendChild(closeBtn);
-
-		tags.push(tag);
-
-		el.insertBefore(tag.element, hiddenInput);
-
-		addMemeKeyword(tag.text);
-
-		refreshTags();
-
-		mainInput.value = "";
+			let tag = {
+				text: mainInput.value,
+				element: document.createElement('span'),
+			};
+	
+			tag.element.classList.add('tag');
+			tag.element.textContent = tag.text;
+			tag.element.setAttribute('style', 'font-size: 15px');
+	
+			let closeBtn = document.createElement('span'); // Create close button on tag
+			closeBtn.classList.add('close');
+			closeBtn.addEventListener('click', function() {
+				removeTag(tags.indexOf(tag));
+			})
+			tag.element.appendChild(closeBtn);
+	
+			tags.push(tag);
+	
+			el.insertBefore(tag.element, hiddenInput);
+	
+			addMemeKeyword(tag.text);
+	
+			addTagHelper(tag.text);
+			console.log(tagsList);
+	
+			mainInput.value = "";
+		} else {
+			document.getElementById('warning').innerHTML = "Can only track 5 memes or less."
+		}
+		
 	};
 
 	el.appendChild(mainInput);
@@ -59,22 +66,27 @@
 		el.removeChild(tag.element);
 
 		deleteMemeKeyword(tag.text);
-		refreshTags();
+		removeTagHelper(tag.text);
 	}
 
-	function refreshTags () { // Variable 'tagsList' contains all current tags
-		let tagsList = [];
-		tags.forEach(function (t) {
-			tagsList.push(t.text);
-		});
+
+	function removeTagHelper(inputtag) {
+		var idx = tagsList.indexOf(inputtag);
+		tagsList.splice(idx, 1);
+	}
+
+
+	function addTagHelper (inputtag) { // Variable 'tagsList' contains all current tags
+		tagsList.push(inputtag);
 		console.log(tagsList);
+		console.log(tagsList.length);
 		hiddenInput.value = tagsList.join(',');
 	}
 })
 
 // Logic for adding keywords
 
-var social_issues = ['Climate Change', 'LGBTQ', 'AIDS', 'Immigration', 'Gun Reform', 'Cancer'];
+var social_issues = ['Climate Change', 'AIDS+', 'Immigration', 'Gun Reform', 'Cancer'];
 let num_of_social_issues = 6;
 
 for (var i = 0; i < num_of_social_issues; i++) {
@@ -83,17 +95,17 @@ for (var i = 0; i < num_of_social_issues; i++) {
 	issue.addEventListener('click', function() {
 		if (this.style.type !== 'submit') {
 			this.style.type = 'submit';
-			this.style.cssText = "background: #ff0000;";
+			this.style.cssText = "background: #5d2d6d; color: white";
 			addSocialIssueKeyword(this.innerHTML);
 		} else {
 			this.style.type = 'button';
-			this.style.cssText = "background: #c19fcc;";
+			this.style.cssText = "background: white;";
+			// #5d2d6d
 			deleteSocialIssueKeyword(this.innerHTML);
 		}
 		console.log("You clicked:", this.innerHTML);
 	});
 }
-
 
 
 function addSocialIssueKeyword(keyword) {
